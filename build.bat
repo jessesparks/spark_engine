4e2e2134568d9692ec3b4aaffda7e914a2a2d9f9
Jesse Sparks <jesse.sparks.cs@gmail.com>
2025-03-09 16:33:56 -0400

    working on Windows using MinGW makefiles

---
+++ build.bat
@@ -0,0 +1,10 @@
+mkdir bin\windows
+mkdir tmp\windows\test_game
+git submodule update --init --recursive
+cmake -S spark_engine -B tmp/windows -G "MinGW Makefiles"
+mingw32-make -C tmp/windows
+copy "%~dp0tmp\windows\spark_engine.lib" "%~dp0bin\windows\"
+cmake -S . -B tmp/windows/test_game -G "MinGW Makefiles"
+mingw32-make -C tmp/windows/test_game
+move "%~dp0tmp\windows\test_game\TestGame.exe" "%~dp0bin\windows\"
+pause
